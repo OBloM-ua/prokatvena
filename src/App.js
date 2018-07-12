3f09c49e27c684a83d1c8b483de7b71154f580fd
Oleh Baidiuk <Oleh12@gmail.com>
2018-07-12 15:06:22 +0200

    add haupt page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import AppHeader from "./components/AppHeader";
+import Body from "./components/Body";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
-                    <p className="App-intro">
-                        <button>Главная</button>
-                        <button>Доп. услуги</button>
-                        <button>Наши преимущества</button>
-                        <button>Страхование</button>
-                        <button>Пересечение границ</button>
-                        <button>Оценка повреждений</button>
-                    </p>
-
-                    <p>
-                        <button>О штрафах</button>
-                        <button>Отличия ПДД</button>
-                        <button>Особенности парковки</button>
-                        <button>О платных дорогах</button>
-                        <button>Контакты</button>
-                        <button>РЕЗЕРВИРОВАНИЕ АВТО</button>
-                    </p>
-                </header>
-
+                <AppHeader/>
+                <Body/>
             </div>
         );
     }
