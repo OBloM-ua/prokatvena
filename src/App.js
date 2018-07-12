cb8a32b1c7e48ac66d44868ed46c7c3b0d317985
Oleh Baidiuk <Oleh12@gmail.com>
2018-07-12 14:48:15 +0200

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                    <p className="App-intro">
+                        <button>Главная</button>
+                        <button>Доп. услуги</button>
+                        <button>Наши преимущества</button>
+                        <button>Страхование</button>
+                        <button>Пересечение границ</button>
+                        <button>Оценка повреждений</button>
+                    </p>
+
+                    <p>
+                        <button>О штрафах</button>
+                        <button>Отличия ПДД</button>
+                        <button>Особенности парковки</button>
+                        <button>О платных дорогах</button>
+                        <button>Контакты</button>
+                        <button>РЕЗЕРВИРОВАНИЕ АВТО</button>
+                    </p>
+                </header>
+
+            </div>
+        );
+    }
 }
 
 export default App;
